638770266d98119a97eeeb9ab6f8531582129b1b
douaem1 <douae.moeniss@gmail.com>
2025-04-05 12:02:34 +0000

    version4

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module org.example.jeu {
   requires javafx.controls;
   requires javafx.fxml;
-  requires javafx.media;  // Essentiel pour MediaPlayer
-  requires javafx.graphics;
 
   opens org.example.jeu to javafx.fxml;
   exports org.example.jeu;
