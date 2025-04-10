1cda6fb62ff61d7de7b665c322292f91cd831e94
douaem1 <douae.moeniss@gmail.com>
2025-04-10 03:21:14 +0100

    jeu threads

--- src/main/java/DAO/users.java
+++ src/main/java/DAO/users.java
@@ -1,53 +1,104 @@
 package DAO;
 
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class users {
     private String username;
     private String password;
 
-public users() {
-}
-public users(String username, String password) {
-    this.username = username;
-    this.password = password;
-}
-public void addUser (users user){
-    try {
-        Statement stm = ConnexionDB.seConnecter();
-        stm.executeUpdate("INSERT INTO users VALUES('" + this.username + "','" + this.password + "')");
-    }catch (Exception e){
-        e.printStackTrace();
+    public users() {
     }
-}
-public String getUsername() {
-    return username;
-}
-public void setUsername(String username) {
-    this.username = username;
-}
-public String getPassword() {
-    return password;
-}
-public void setPassword(String password) {
-    this.password = password;
-}
-public void DeleteUser(users user) {
-    try {
-        Statement stm = ConnexionDB.seConnecter();
-        stm.executeUpdate("DELETE FROM users WHERE username='" + this.username + "'");
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-}
-public void UpdateUser(users user) {
-    try {
-        Statement stm = ConnexionDB.seConnecter();
-        stm.executeUpdate("UPDATE users SET password='" + this.password + "' WHERE username='" + this.username + "'");
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-}
 
-}
+    public users(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
+    public void addUser(users user) {
+        try {
+            Statement stm = ConnexionDB.seConnecter();
+            stm.executeUpdate("INSERT INTO users VALUES('" + this.username + "','" + this.password + "')");
+            if (stm == null) {
+                System.out.println("erreur de connexion");
+                return;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public void DeleteUser(users user) {
+        try {
+            Statement stm = ConnexionDB.seConnecter();
+            stm.executeUpdate("DELETE FROM users WHERE username='" + this.username + "'");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void UpdateUser(users user) {
+        try {
+            Statement stm = ConnexionDB.seConnecter();
+            stm.executeUpdate("UPDATE users SET password='" + this.password + "' WHERE username='" + this.username + "'");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<users> getAllUsers() {
+        List<users> ListUsers = new ArrayList<users>();
+        try {
+            Statement stm = ConnexionDB.seConnecter();
+            ResultSet rs = stm.executeQuery("select * from users");
+            while (rs.next()) {
+                String username = rs.getString(1);
+                String password = rs.getString(2);
+                ListUsers.add(new users(username, password));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ListUsers;
+    }
+
+    public boolean verifyUser(String username, String password) {
+        List<users> ListUsers = getAllUsers();
+        for (users user : ListUsers) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean userExists(String username) {
+        List<users> ListUsers = getAllUsers();
+        for (users user : ListUsers) {
+            if (user.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/main/java/chat_Client_Serveur/Serveur.java
+++ src/main/java/chat_Client_Serveur/Serveur.java
@@ -2,32 +2,54 @@ package chat_Client_Serveur;
 
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.*;
 import java.io.*;
-import java.net.*;
 
+//listening to clients who wish to connect and when they do it will spawn a new thread to handle them
+//each object of this class will be responsible for communicating with a client
+//the client class will implement the interface runnable(each instance of this class will be excecuted by a separate thread)
 public class Serveur {
-    private static final int PORT = 7234;
-    private static List<Client> clients = new ArrayList<>();
 
-    public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.println("Serveur start");
-            while (true) {
-                Socket clientsocket = serverSocket.accept();
-                System.out.println("New client connected" + clientsocket);
-                Client clientThread = new Client(clientsocket);
-                clients.add(clientThread);
-                new Thread(clientThread).start();
+    private ServerSocket serverSocket;//this object will be responsible for listening for incoming cnxs(clients)and creating a socket object to communicate with them
+    //constructeur
+    public Serveur(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+    //methode:keep our server running
+    public void startServeur() {
+        //input output error handling
+        try {
+            while (!serverSocket.isClosed()) {//while the serversocket isn't close we're gonna wait for a client to connect by a serversocket that accept methode
+                //blocking methode : our methode will be stopped here until a client connects
+                //when a client connects a socket object is returned wich used to communicate with the client
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected to the server !");
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
             }
+        }catch (IOException e){
 
+        }
+
+    }
+    //avoid nested try catches(if an error accures we are gonna shut down our serversocket)
+    public void closeServerSocket() {
+        try {
+            //make sure that our server socket is not null(cause if it is we're gonna have a null pointer exception)
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
         }catch (IOException e){
             e.printStackTrace();
         }
     }
-    public static void broadcast(String message) {
-        for (Client client : clients) {
-            client.sendMessage(message);
-        }
+    //main methode to intantiate object from it and run it
+    public static void main(String[] args) throws IOException {
+        //port number is 7103: it is saying that our server will be listening for clients that are making cnx to this port num
+        //client will say hey i wanna talk on port num 7103
+        ServerSocket serverSocket = new ServerSocket(7103);
+        Serveur serveur = new Serveur(serverSocket);
+        serveur.startServeur();//appel of function that we made up
     }
+
 }
