f0bfc1b392bc0b468afe90dc7010ad1d42e40048
Oumaima <oumaima.ameziane8@gmail.com>
2025-05-01 17:34:35 +0100

    version2

--- src/main/java/DAO/Scores.java
+++ src/main/java/DAO/Scores.java
@@ -5,17 +5,17 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class scores {
+public class Scores {
 
     private String username;
     private int score;
     private int round_duration;
     private String round_date;
 
-    public scores(){
+    public Scores(){
     }
 
-    public scores( String username, int score, int round_duration, String round_date) {
+    public Scores( String username, int score, int round_duration, String round_date) {
 
         this.username = username;
         this.score = score;
@@ -49,10 +49,10 @@ public class scores {
     }
 
     // ajouter un score
-    public void addScore(scores new_score){
+    public static void addScore(Scores new_score){
         try {
             Statement stm = ConnexionDB.seConnecter();
-            stm.executeUpdate("INSERT INTO scores (username, score, round_duration, round_date) VALUES ('" + new_score.getUsername() + "'," + new_score.getScore() + "," + new_score.getRoundDuration() + ", CURRENT_TIMESTAMP)");
+            stm.executeUpdate("INSERT INTO Scores (username, score, round_duration, round_date) VALUES ('" + new_score.getUsername() + "'," + new_score.getScore() + "," + new_score.getRoundDuration() + ", CURRENT_TIMESTAMP)");
             if (stm == null) {
                 System.out.println("Erreur de connexion à la base de données ! ");
                 return;
@@ -62,20 +62,20 @@ public class scores {
         }
     }
 
-    // recuperer tous les scores par user
+    // recuperer tous les Scores par user
 
-    public List<scores> UsernameScore(String username){
-        List<scores> ListScores = new ArrayList<scores>();
+    public List<Scores> UsernameScore(String username){
+        List<Scores> ListScores = new ArrayList<Scores>();
         try{
             // ouvrir la connexion a la base de données
             // Statement eest pour executer des requete SQL
             // recuperer les resultats de excuteQuery par ResultSet
             //stm.excuteQuery
             Statement stm = ConnexionDB.seConnecter();
-            ResultSet rs=stm.executeQuery("SELECT * From scores WHERE username='"+username+"'ORDER BY round_date DESC");
+            ResultSet rs=stm.executeQuery("SELECT * From Scores WHERE username='"+username+"'ORDER BY round_date DESC");
             // tant qu'il reste des resultats a lire
             while (rs.next()){
-                scores score = new scores();
+                Scores score = new Scores();
                 score.setUsername(rs.getString("username"));
                 score.setScore(rs.getInt("score"));
                 score.setRoundDuration(rs.getInt("round_duration"));
@@ -89,10 +89,10 @@ public class scores {
         return ListScores;
     }
     //supprimmer un score de la liste
-    public void DeleteScore(scores score){
+    public void DeleteScore(Scores score){
         try {
             Statement stm = ConnexionDB.seConnecter();
-            stm.executeUpdate("DELETE FROM scores WHERE username='" + score.getUsername() + "'");
+            stm.executeUpdate("DELETE FROM Scores WHERE username='" + score.getUsername() + "'");
         } catch (Exception e) {
             e.printStackTrace();
         }
